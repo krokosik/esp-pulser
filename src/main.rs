e9cb6b4c98800ca35d38d1269dc8f7464584a33f
krokosik <wikkra@gmail.com>
2024-12-27 11:14:12 +0000

    Working SPI display, remove eth example code for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use anyhow::{anyhow, Ok};
+use anyhow::anyhow;
 use display_interface_spi::SPIInterface;
-use embedded_graphics::{pixelcolor::Rgb565, prelude::*};
-use esp_idf_svc::eventloop::EspSystemEventLoop;
-use esp_idf_svc::timer::EspTaskTimerService;
+use embedded_graphics::primitives::PrimitiveStyleBuilder;
+use embedded_graphics::{pixelcolor::Rgb565, prelude::*, primitives::Rectangle};
+use mipidsi::options::*;
 use mipidsi::{models::ST7789, options::Orientation, Builder};
 
-use esp_idf_svc::eth;
 use esp_idf_svc::hal::{delay, gpio::*, prelude::*, spi, units::FromValueType};
-use esp_idf_svc::{ipv4, ping};
 
-use log::{info, warn};
+use log::info;
 
 fn main() -> Result<(), anyhow::Error> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -19,8 +17,6 @@ fn main() -> Result<(), anyhow::Error> {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let sysloop = EspSystemEventLoop::take()?;
-    let timer_service = EspTaskTimerService::new()?;
     let peripherals = Peripherals::take()?;
     let pins = peripherals.pins;
 
@@ -30,7 +26,7 @@ fn main() -> Result<(), anyhow::Error> {
     let sdo = pins.gpio35; // mosi
     let sdi = pins.gpio37; // miso
     let tft_cs = pins.gpio42;
-    let eth_cs = pins.gpio10;
+    // let eth_cs = pins.gpio10;
     let rst = PinDriver::output(pins.gpio41)?;
 
     let mut tft_power = PinDriver::output(pins.gpio7)?;
@@ -52,22 +48,10 @@ fn main() -> Result<(), anyhow::Error> {
         sclk,
         sdo,
         Some(sdi),
-        &spi::SpiDriverConfig::new().dma(spi::Dma::Auto(4096)),
+        &spi::SpiDriverConfig::new().dma(spi::Dma::Auto(240 * 135 * 2 + 8)),
     )?;
 
     let tft_spi_device = spi::SpiDeviceDriver::new(&spi_driver, Some(tft_cs), &config)?;
-
-    let mut eth = eth::EspEth::wrap(eth::EthDriver::new_spi(
-        &spi_driver,
-        pins.gpio27,
-        Some(eth_cs),
-        None::<AnyIOPin>,
-        eth::SpiEthChipset::W5500,
-        20_u32.MHz().into(),
-        Some(&[0x02, 0x00, 0x00, 0x12, 0x34, 0x56]),
-        None,
-        sysloop.clone(),
-    )?)?;
 
     info!("SPI initialized");
 
@@ -75,8 +59,10 @@ fn main() -> Result<(), anyhow::Error> {
 
     let di = SPIInterface::new(tft_spi_device, dc);
     let mut display = Builder::new(ST7789, di)
-        .display_size(240, 135)
-        .orientation(Orientation::new())
+        .display_size(135, 240)
+        .display_offset(52, 40)
+        .invert_colors(ColorInversion::Inverted)
+        .orientation(Orientation::new().rotate(Rotation::Deg180))
         .reset_pin(rst)
         .init(&mut delay)
         .map_err(|_| anyhow!("display init"))?;
@@ -87,41 +73,20 @@ fn main() -> Result<(), anyhow::Error> {
         .clear(Rgb565::RED)
         .map_err(|_| anyhow!("clear display"))?;
 
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_color(Rgb565::BLACK)
+        .stroke_width(3)
+        .fill_color(Rgb565::GREEN)
+        .build();
+
+    Rectangle::new(Point::new(0, 0), Size::new(100, 100))
+        .into_styled(style)
+        .draw(&mut display)
+        .map_err(|_| anyhow!("draw rectangle"))?;
+
     info!("Display cleared");
 
-    // Wait for the Eth peripheral and network layer 3 to come up - in an async way because we can
-    let ip_info = esp_idf_svc::hal::task::block_on(async {
-        let mut eth = eth::AsyncEth::wrap(&mut eth, sysloop.clone(), timer_service)?;
-
-        info!("Starting eth...");
-
-        eth.start().await?;
-
-        info!("Waiting for DHCP lease...");
-
-        eth.wait_netif_up().await?;
-
-        let ip_info = eth.eth().netif().get_ip_info()?;
-
-        info!("Eth DHCP info: {:?}", ip_info);
-
-        Ok(ip_info)
-    })?;
-
-    ping(ip_info.subnet.gateway)?;
-
-    Ok(())
-}
-
-fn ping(ip: ipv4::Ipv4Addr) -> Result<(), anyhow::Error> {
-    info!("About to do some pings for {:?}", ip);
-
-    let ping_summary = ping::EspPing::default().ping(ip, &Default::default())?;
-    if ping_summary.transmitted != ping_summary.received {
-        warn!("Pinging IP {} resulted in timeouts", ip);
+    loop {
+        delay::FreeRtos::delay_ms(1000);
     }
-
-    info!("Pinging done");
-
-    Ok(())
 }
