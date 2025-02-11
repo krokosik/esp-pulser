6dd3ad43c0a6dfb4d4af74dc0480ffcde6924512
krokosik <wikkra@gmail.com>
2025-02-11 23:01:39 +0000

    Change binary filenames to eliminate a breaking change in updates

--- src/ota.rs
+++ src/ota.rs
@@ -8,7 +8,7 @@ use esp_idf_svc::sys::{ESP_ERR_IMAGE_INVALID, ESP_ERR_INVALID_RESPONSE};
 use http::{header::ACCEPT, Uri};
 
 pub const UPDATE_BIN_URL: &str =
-    "https://github.com/krokosik/esp-pulser/releases/download/vTAG/esp-pulser-ota";
+    "https://github.com/krokosik/esp-pulser/releases/download/vTAG/esp-pulser";
 
 const FIRMWARE_DOWNLOAD_CHUNK_SIZE: usize = 1024 * 20;
 // Not expect firmware bigger than partition size
